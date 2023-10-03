1522dcb66a123261feecd3ba0de84464ad92a400
Anton <tagil.anton@yandex.ru>
2023-10-03 18:58:23 +0300

    Второй вариант решения

--- Python_S3/S3.1.py
+++ Python_S3/S3.1.py
@@ -4,5 +4,11 @@
 # Output: 6
 
 data = [int(i) for i in input('Введите числа: ').split()]
+# print(len(set(data)))
 
-print(len(set(data)))
+d = {}
+for i in data:
+    if i not in d:
+        d[i] = 0
+    d[i] += 1
+print(len(d.keys()))
